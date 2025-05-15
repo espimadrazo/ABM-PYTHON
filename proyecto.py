1a130c891d04a79469dd30f352c3f09e896539a3
Espi Madrazo <espimadrazo@gmail.com>
2025-05-14 22:47:08 -0300

    Fallando

--- proyecto.py
+++ proyecto.py
@@ -6,10 +6,7 @@ modelo_producto = {
     "Precio": None
 }
 
-numerador= 0
-
 """
-FIXME agregar validación de datos a todos los inputs (lower, entero) if vacio or type num or "Ingrese un valor válido"
 FIXME ver lo del numerador (que cada producto tenga su número en la lista)
 """
 
@@ -29,29 +26,31 @@ while True:
                 nuevo_producto = modelo_producto.copy()
                 numerador = 0
 
-                nuevo_producto["Nombre"] = input("Ingrese el nombre del producto").strip()
+                nuevo_producto["Nombre"] = input("Ingrese el nombre del producto").strip().lower()
                 if nuevo_producto["Nombre"] == "":
                     print("Ingrese un nombre válido")
                     continue
                 else:
                     print("Carga exitosa")
 
-                nuevo_producto["Categoría"] = input("Ingrese la categoría del producto").strip()
+                nuevo_producto["Categoría"] = input("Ingrese la categoría del producto").strip().lower()
                 if nuevo_producto["Categoría"] == "":
                     print("Ingrese una categoría válida")
                     continue
                 else:
                     print("Carga exitosa")
 
-                nuevo_producto["Precio"] = int(input("Ingrese el precio del producto (sin centavos)").strip())
-                if nuevo_producto["Precio"] == "":
+                nuevo_producto["Precio"] = input("Ingrese el precio del producto sin centavos").strip()
+                if (nuevo_producto["Precio"].isdigit()):
+                    nuevo_producto["Precio"] = int(nuevo_producto["Precio"])
+                    if nuevo_producto["Precio"] <= 0:
+                        print("El precio no puede ser 0 o negativo")
+                        continue
+                else:
                     print("Ingrese un precio válido")
                     continue
-                else:
-                    print("Carga exitosa")
-                
+
                 productos.append(nuevo_producto)
-                numerador += 1
 
                 carga_producto = input("¿Desea ingresar otro producto? S/N").strip().lower()
                 if carga_producto == "s":
@@ -67,12 +66,21 @@ while True:
 
         case 2:
             print("Lista total de productos:")
-            for producto in productos:
-                print(f"{numerador}. {producto}")
+            for index, x in enumerate(productos, start=1):
+                print(index, x)
 
         case 3:
-            busqueda = input("Ingrese el nombre del producto que desea buscar").strip()
-
+            busqueda = input("Ingrese el nombre del producto que desea buscar").strip().lower()
+            if busqueda == "":
+                print("Ingrese un nombre válido")
+                continue
+            else:
+                for producto in productos:
+                    if busqueda == producto{"Nombre"}
+                    print(producto)
+                else:
+                    print("Producto no encontrado")
+         
         case 4:
             eliminar = input("Ingrese el número del producto que desea eliminar").strip()
 
