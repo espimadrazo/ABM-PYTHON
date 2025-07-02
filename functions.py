b7b2c25f8c073f5ceff2c3a081e4b730836521f2
Espi Madrazo <espimadrazo@gmail.com>
2025-07-01 21:51:39 -0300

    Integración con base de datos sqlite

--- functions.py
+++ functions.py
@@ -1,86 +1,187 @@
+import colorama
+import sqlite3
+
 # MENU
 def menu():
     print("--Sistema de Gestión Básica De Productos--")
     print("1. Agregar producto")
     print("2. Mostrar productos")
     print("3. Buscar producto")
-    print("4. Eliminar producto")
-    print("5. Salir")
+    print("4. Modificar precio de producto")
+    print("5. Eliminar producto")
+    print("6. Salir")
 
 # AGREGAR PRODUCTO
 def agregar_producto():
-    import colorama
-    colorama.init()
+    try:
+        colorama.init()
 
-    while True:
-        nombre = input("Ingrese el nombre del producto: ").strip().lower()
-        if nombre == "":
-            print(colorama.Fore.RED + "Ingrese un nombre" + colorama.Style.RESET_ALL)
-            continue
-        else:
-            print(colorama.Fore.GREEN + "Carga exitosa" + colorama.Style.RESET_ALL)
+        while True:
+            conexion = sqlite3.connect("productos.db")
+            cursor = conexion.cursor()
 
-        categoria = input("Ingrese la categoría del producto: ").strip().lower()
-        if categoria == "":
-            print(colorama.Fore.RED + "Ingrese una categoría" + colorama.Style.RESET_ALL)
-            continue
-        else:
-            print(colorama.Fore.GREEN + "Carga exitosa" + colorama.Style.RESET_ALL)
-
-        precio = input("Ingrese el precio del producto sin centavos: ").strip()
-        if precio.isdigit():
-            precio = int(precio)
-            if precio <= 0:
-                print(colorama.Fore.RED + "El precio no puede ser 0 o negativo" + colorama.Style.RESET_ALL)
+            nombre = input("Ingrese el nombre del producto: ").strip().lower()
+            if nombre == "":
+                print(colorama.Fore.RED + "Ingrese un nombre" + colorama.Style.RESET_ALL)
                 continue
-        else:
-            print(colorama.Fore.RED + "Ingrese un precio válido" + colorama.Style.RESET_ALL)
-            continue
+            else:
+                print(colorama.Fore.GREEN + "Carga exitosa" + colorama.Style.RESET_ALL)
 
-        nuevo_producto = [nombre, categoria, precio]
-        return nuevo_producto
+            categoria = input("Ingrese la categoría del producto: ").strip().lower()
+            if categoria == "":
+                print(colorama.Fore.RED + "Ingrese una categoría" + colorama.Style.RESET_ALL)
+                continue
+            else:
+                print(colorama.Fore.GREEN + "Carga exitosa" + colorama.Style.RESET_ALL)
+
+            precio = input("Ingrese el precio del producto sin '$' y sin centavos: ").strip()
+            if precio.isdigit():
+                precio = int(precio)
+                if precio <= 0:
+                    print(colorama.Fore.RED + "El precio no puede ser 0 o negativo" + colorama.Style.RESET_ALL)
+                    continue
+            else:
+                print(colorama.Fore.RED + "Ingrese un precio válido" + colorama.Style.RESET_ALL)
+                continue
+
+            cursor.execute("INSERT INTO productos (nombre, categoría, precio) VALUES(?, ?, ?)", (nombre, categoria, precio))
+            print(colorama.Fore.GREEN + "Carga final de producto exitosa" + colorama.Style.RESET_ALL)
+
+            pregunta = input("¿Desea agregar otro producto? S/N").lower()
+            if pregunta == "s":
+                continue
+            elif pregunta == "n":
+                break
+            else:
+                print("Ingrese una opción válida")
+
+        conexion.commit()
+
+    except sqlite3.Error as e:
+        print(f"Error: {e}")
+        conexion.rollback()
+
+    finally:
+        conexion.close()
 
 # MOSTRAR PRODUCTOS
-def mostrar_productos(productos):
-    print("Lista total de productos:")
-    for index, producto in enumerate(productos, start = 1):
-        print(f"{index}.\t{producto[0]}\t{producto[1]}\t{producto[2]}")
+def mostrar_productos():
+    try: 
+        colorama.init()
+
+        conexion = sqlite3.connect("productos.db")
+        cursor = conexion.cursor()
+
+        cursor.execute("SELECT * FROM productos")
+        lista_productos = cursor.fetchall()
+        print("Lista total de productos:")
+        for producto in lista_productos:
+            print(f"ID: {producto[0]}, Nombre: {producto[1]}, Categoría: {producto[2]}, Precio: {producto[3]}")
+    
+    finally:
+        conexion.close()
 
 # BUSCAR PRODUCTOS
-def buscar_productos(productos):
-    import colorama
-    colorama.init()
+def buscar_productos():
+    try: 
+        colorama.init()
+        conexion = sqlite3.connect("productos.db")
+        cursor = conexion.cursor()
 
-    busqueda = input("Ingrese el nombre del producto que desea buscar: ").strip().lower()
-    if busqueda == "":
-        print(colorama.Fore.RED + "Ingrese un nombre válido" + colorama.Style.RESET_ALL)
-    else:
-        for producto in productos:
-            if busqueda == producto[0]:
-                print(f"{producto[0]}\t{producto[1]}\t{producto[2]}")
-            else:
-                print(colorama.Fore.RED + "Producto no encontrado" + colorama.Style.RESET_ALL)
+        cursor.execute("SELECT * FROM productos")
+        lista_productos = cursor.fetchall()
+        busqueda = input("Ingrese el nombre del producto que desea buscar: ").strip().lower()
+        if busqueda == "":
+            print(colorama.Fore.RED + "Ingrese un nombre" + colorama.Style.RESET_ALL)
+        else:
+            for producto in lista_productos:
+                if busqueda == producto[1]:
+                    print(f"ID: {producto[0]}, Nombre: {producto[1]}, Categoría: {producto[2]}, Precio: {producto[3]}")
+                else:
+                    print(colorama.Fore.RED + "Producto no encontrado" + colorama.Style.RESET_ALL)
+    finally:
+        conexion.close()
 
-# ELIMINAR PRODUCTO
-def eliminar_producto(productos):
-    import colorama
-    colorama.init()
-
+# MODIFICAR PRECIO
+def modificar_precio():
     try:
-        eliminar = int(input("Ingrese el número del producto que desea eliminar según su índice: "))
-        eliminar = (eliminar) - 1
-        productos.pop(eliminar)
-        print(colorama.Fore.GREEN + "Eliminación exitosa" + colorama.Style.RESET_ALL)
+        colorama.init()
+        conexion = sqlite3.connect("productos.db")
+        cursor = conexion.cursor()
+
+        while True:
+            id = input("Ingrese el número de ID del producto que desea actualizar: ")
+            if id.isdigit():
+                id = int(id)
+            else:
+                print(colorama.Fore.RED + "Ingrese un ID válido" + colorama.Style.RESET_ALL)
+                continue
+
+            nuevo_precio = input("Ingrese el nuevo precio del producto sin '$' y sin centavos: ")
+            if nuevo_precio.isdigit():
+                nuevo_precio = int(nuevo_precio)
+                if nuevo_precio <= 0:
+                    print(colorama.Fore.RED + "El precio no puede ser 0 o negativo" + colorama.Style.RESET_ALL)
+                    continue
+                else:
+                    cursor.execute("UPDATE productos SET precio = ? WHERE id = ?", (nuevo_precio, id))
+                    print(colorama.Fore.GREEN + "Carga exitosa" + colorama.Style.RESET_ALL)
+                    break
+            else:
+                print(colorama.Fore.RED + "Ingrese un precio válido" + colorama.Style.RESET_ALL)
+                continue
+
+        conexion.commit()
+
     except ValueError:
         print(colorama.Fore.RED + "Ingrese un número" + colorama.Style.RESET_ALL)
+
     except IndexError:
         print(colorama.Fore.RED + "Ingrese un número válido de índice" + colorama.Style.RESET_ALL)
 
+    except sqlite3.Error as e:
+        print(f"Error: {e}")
+        conexion.rollback()
+
+    finally:
+        conexion.close()
+
+# ELIMINAR PRODUCTO
+def eliminar_producto():
+    colorama.init()
+
+    try:
+        conexion = sqlite3.connect("productos.db")
+        cursor = conexion.cursor()
+
+        cursor.execute("BEGIN TRANSACTION")
+
+        id = int(input("Ingrese el número de ID del producto que desea eliminar: "))
+        cursor.execute("DELETE FROM productos WHERE id = ?", (id,))
+        print(colorama.Fore.GREEN + "Eliminación exitosa" + colorama.Style.RESET_ALL)
+
+        conexion.commit()
+
+    except ValueError:
+        print(colorama.Fore.RED + "Ingrese un número" + colorama.Style.RESET_ALL)
+
+    except IndexError:
+        print(colorama.Fore.RED + "Ingrese un número válido de índice" + colorama.Style.RESET_ALL)
+
+    except sqlite3.Error as e:
+        print(f"Error: {e}")
+        conexion.rollback()
+
+    finally:
+        conexion.close()
 
 # TERMINAR PROGRAMA
 def terminar_programa():
     print("Gestión finalizada")
 
+# CASE _
+def imprimir_error_opcion():
+    print(colorama.Fore.RED + "Ingrese una opción válida" + colorama.Style.RESET_ALL)
 
 
 
--- proyectofinal.py
+++ proyectofinal.py
@@ -1,42 +1,60 @@
+import sqlite3
 import functions
 import colorama
 
-# Variables de almacenamiento
-productos = []
+# Conexión a base de datos
+connection = sqlite3.connect("productos.db")
+cursor = connection.cursor()
+
+# Creación de tabla en base de datos
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS productos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT NOT NULL,
+        categoría TEXT NOT NULL,
+        precio INTEGER NOT NULL
+    )
+""")
 
 # Programa
 while True:
     functions.menu()
 
-    opcion_menu= int(input("Ingrese el número de la opción que desea elegir: ").strip())
+    try:
+        opcion_menu= int(input("Ingrese el número de la opción que desea elegir: ").strip())
+        match opcion_menu:
+            # Agregar producto
+            case 1:
+                producto = functions.agregar_producto()
 
-    match opcion_menu:
+            # Mostrar productos
+            case 2:
+                functions.mostrar_productos()
 
-        # Agregar producto
-        case 1:
-            producto = functions.agregar_producto()
-            productos.append(producto)
-            print(colorama.Fore.GREEN + "Carga final de producto exitosa, para agregar otro vuelva a seleccionar opción 1" + colorama.Style.RESET_ALL)
+            # Buscar producto
+            case 3:
+                functions.buscar_productos()
 
-        # Mostrar productos
-        case 2:
-            functions.mostrar_productos(productos)
+            # Modificar precio producto
+            case 4:
+                functions.mostrar_productos()
+                functions.modificar_precio()
 
-        # Buscar producto
-        case 3:
-            functions.buscar_productos(productos)
+            # Eliminar producto
+            case 5:
+                functions.mostrar_productos()
+                functions.eliminar_producto()
 
-        # Eliminar producto
-        case 4:
-            functions.mostrar_productos(productos)
-            functions.eliminar_producto(productos)
+            # Salir
+            case 6:
+                functions.terminar_programa()
+                break
 
-        # Salir
-        case 5:
-            functions.terminar_programa()
-            break
+            case _:
+                functions.imprimir_error_opcion()
 
-        case _:
-            print("Ingrese una opción válida")
-            continue
+    except ValueError:
+        print(colorama.Fore.RED + "Ingrese un número" + colorama.Style.RESET_ALL)
+
+
 
